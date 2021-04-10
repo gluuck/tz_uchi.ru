2044356e12d29e6d3fea47b05a046152d6b47ddf
gluuck <ostapenkov74@gmail.com>
2021-04-10 17:42:48 +0500

    ohoho

--- faringate.rb
+++ faringate.rb
@@ -1,20 +1,34 @@
-class Object
-  def is_number?
-    to_f.to_s == to_s || to_i.to_s == to_s
+# class Object
+#   def is_number?
+#     to_f.to_s == to_s || to_i.to_s == to_s
+#   end
+# end
+#
+# loop do
+#   print 'Ведите градусы С или stop для выхода: '
+#     val = gets.chomp
+#     break if val == 'stop'
+#
+#   if val.is_number?
+#       val = Integer(val)
+#       faringate =  9/5 * val  + 32
+#       puts "Градусы по фарингейту #{faringate} F "
+#   else
+#     puts 'Это не число, введите число'
+#   end
+#
+# end
+
+#Цикл работает выход тоже , проверка не работает , потому что строку to_i
+#переопределяет на 0 вот почему в начале делал Object
+def faringate
+  loop do
+    print 'Ведите градусы С или stop для выхода: '
+    val = gets.chomp
+    break if val == 'stop'
+    far = 9/5 * val.to_i  + 32
+    puts "Температура по фарингейту #{far} F"
   end
 end
 
-loop do
-  print 'Ведите градусы С или stop для выхода: '
-    val = gets.chomp
-    break if val == 'stop'
-    
-  if val.is_number?
-      val = Integer(val)        
-      faringate = (val * 9/5) + 32
-      puts "Градусы по фарингейту #{faringate} F "       
-  else 
-    puts 'Это не число, введите число'  
-  end  
-
-end
+faringate
--- min_max.rb
+++ min_max.rb
@@ -1,7 +1,64 @@
-mass = [1, 2, 12, 34, 35, 6, 0, 34, 122, 124, 789, 999, 33, 54, 763, 893 ] 
+mass = [1, 2, 12, 34, 35, 6, 0, 34, 122, 124, 789, 999, 33, 54, 763, 893 ]
 def min_max(arr)
    puts arr.max(2)
    puts '=========='
    puts arr.min(2)
 end
 min_max(mass)
+
+#p mass.sort.shift(2)    говорят сортировка зло )
+#p mass.sort.pop(2)
+
+
+#ребята подсказали сам не напишу еще такое пока что
+def sort(mass)
+  n = mass.length
+    loop do
+    swapped = false
+        (n-1).times do |i|
+      if mass[i] > mass[i + 1]
+        mass[i], mass[i + 1] = mass[i + 1], mass[i]
+        swapped = true
+      end
+    end
+        break if not swapped
+  end
+   mass
+end
+p sort(mass).shift(2)
+
+def two_max(mass)
+  m1 = mass[0]
+  m2 = mass[0]
+
+  mass.each do |elem|
+    if elem > m1
+      tmp = m1
+      m1 = elem
+      m2 = tmp if tmp > m2
+    elsif elem > m2
+      m2 = elem
+    end
+  end
+
+  [m1, m2]
+end
+
+p two_max(mass)
+
+
+def two_min(mass)
+  n1 = mass[0]
+  n2 = mass[0]
+  mass.each do |e|
+    if e < n1
+      tmp = n1
+      n1 = e
+      n2 = tmp if tmp < n2
+    elsif e < n2
+      n2 = e
+    end
+   end
+  [n1, n2]
+end
+p two_min(mass)
